836516f8f8b88b40c1f8b9a54408d0e9993aa99f
Oskar Strömberg <oskstr@kth.se>
2018-05-14 17:30:31 +0200

    Handle boundaries and images.

--- src/se/kth/inda17/Character.java
+++ src/se/kth/inda17/Character.java
@@ -2,19 +2,29 @@ package se.kth.inda17;
 
 import javafx.geometry.Point2D;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+
 
 abstract class Character {
     private Image image;
-    private Point2D position;
-    private int boundaryWidth;
-    private int boundaryHeight;
+    private int width;
+    private int height;
 
-    Character(String image, Point2D position, int boundaryWidth, int boundaryHeight) {
+    private Point2D position;
+
+    private static int boundaryWidth;
+    private static int boundaryHeight;
+
+    Character(String image, int width, int height, Point2D position, int boundaryWidth, int boundaryHeight) {
         this.position = position;
-        this.boundaryWidth = boundaryWidth;
-        this.boundaryHeight = boundaryHeight;
-        this.image = new Image(image);
+
+        Character.boundaryWidth = boundaryWidth;
+        Character.boundaryHeight = boundaryHeight;
+
+        this.image = new Image(getResource(image));
+        this.width = width;
+        this.height = height;
     }
 
     public Point2D getPosition() {
@@ -26,8 +36,26 @@ abstract class Character {
     }
 
     public boolean isOutOfBounds() {
-        return (position.getX() < 0             || position.getY() < 0               ||
-                position.getX() > boundaryWidth || position.getY() > boundaryHeight);
+        return (position.getX() < 0                     || position.getY() < 0                        ||
+                position.getX() + width > boundaryWidth || position.getY() + height > boundaryHeight);
+    }
+
+    void stayInBounds() {
+        if (isOutOfBounds()) {
+            double x = position.getX();
+            double y = position.getY();
+            double w = boundaryWidth - width;
+            double h = boundaryHeight - height;
+
+            if (x < 0)  setPosition(new Point2D(0,position.getY()));
+            if (x > w)  setPosition(new Point2D(w,position.getY()));
+            if (y < 0)  setPosition(new Point2D(position.getX(),0));
+            if (y > h)  setPosition(new Point2D(position.getX(),h));
+        }
+    }
+
+    void render(GraphicsContext gc) {
+        gc.drawImage(image, position.getX(), position.getY(), width, height);
     }
 
     private Rectangle2D getBoundary() {
@@ -36,5 +64,17 @@ abstract class Character {
 
     public boolean isCollidingWith(Character c) {
         return getBoundary().intersects(c.getBoundary());
+    }
+
+    private static String getResource(String filename) {
+        return Player.class.getResource(filename).toString();
+    }
+
+    static int getBoundaryWidth() {
+        return boundaryWidth;
+    }
+
+    static int getBoundaryHeight() {
+        return boundaryHeight;
     }
 }
